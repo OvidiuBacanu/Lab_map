b3ff4adeec1affb04686d51e6adade5265139c73
OvidiuBacanu <obacanu96@gmail.com>
2020-12-13 22:33:48 +0200

    Am sters comentariile

--- src/main/java/socialnetwork/Main.java
+++ src/main/java/socialnetwork/Main.java
@@ -5,13 +5,3 @@ public class Main {
         MainFX.main(args);
     }
 }
-
-
-//comentariu 1 Ovidiu
-//comentariu 2 Ovidiu
-//comentariu 3 Ovidiu
-
-// comentariu 1 Paul
-// comentariu 2 Paul
-// comentariu 3 Paul
-
